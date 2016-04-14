2614ec411bc519663c159ca20368824a7da5f668
Sibelius Seraphini <sibeliusseraphini@gmail.com>
2016-04-14 19:06:13 -0300

    echo bot v1

--- server/api/routes/bot.js
+++ server/api/routes/bot.js
@@ -1,3 +1,34 @@
+import FacebookMessenger from 'fb-messenger';
+
+const verify_token = 'supersecrecttoken';
+const token = 'your secret token';
+
+const messenger = new FacebookMessenger(token);
+
 export default (router) => {
-  // TODO add get and post webhook facebook
+  router
+    .get('/webhook', async ctx => {
+      const challenge = ctx.request.query['hub.challenge'];
+      if (challenge === verify_token) {
+        ctx.status = 200;
+        ctx.body = challenge;
+      }
+    })
+
+    // handle incoming messages
+    .post('/webhook', async ctx => {
+      console.log(ctx.request.body);
+
+      const messagingEvents = ctx.request.body.entry[0].messaging;
+
+      messagingEvents.map(event => {
+        const sender = event.sender.id
+        if (event.message && event.message.text) {
+          messenger.sendTextMessage(sender, "Echo: " + event.message.text);
+        }
+      });
+
+      ctx.status = 200;
+      ctx.body = 'ok';
+    })
 }
